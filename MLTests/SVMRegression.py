4b3e2da7094d9de6575f9fcee6c2b29e4a681972
RavinduHasithanjana <ravinduceylofana@outlook.com>
2018-04-20 22:49:08 +0530

    checking the accuracy level of the regression algorithm

--- MLTests/SVMRegression.py
+++ MLTests/SVMRegression.py
@@ -5,6 +5,7 @@ import numpy
 from sklearn.svm import SVR
 from sklearn import svm
 from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.metrics import explained_variance_score
 
 def readcsv ():
     df = pd.read_csv('/Users/ravinduperera/Desktop/IIT/Research/Development/Dev/csvfile.csv', header=None)
@@ -20,16 +21,22 @@ def regressionalgo (cc):
     print(data.shape)
     # print(target.shape)
     # #
-    X = data[:100]
-    y = target[:100]
+    X = data
+    y = target
 
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     X.reshape(-1, 1), y, test_size=0.5, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X.reshape(-1, 1), y, test_size=0.5, random_state=0)
 
     clf = svm.SVR()
-    clf.fit(X.reshape(-1,1), y)
+    clf.fit(X_train,y_train)
     SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1, gamma='auto',
-        kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+        kernel='linear', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+    pred = clf.predict(X_test)
+
+    # print(pred)
+
+    acc = explained_variance_score(pred, y_test,multioutput='raw_values')
+    print(acc)
 
 
 if __name__ == '__main__': readcsv()
