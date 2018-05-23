aae1efaa959a0c27a721f25460c68bbb67037329
RavinduHasithanjana <ravinduceylofana@outlook.com>
2018-05-23 11:31:58 +0530

    added the sample files

--- ThrotaleMLSystem/Throtale.py
+++ ThrotaleMLSystem/Throtale.py
@@ -6,10 +6,15 @@ from sklearn.externals import joblib
 
 
 
-def predictions ():
+def predictions():
+    print()
+    clf = joblib.load('throtale.pkl')
+    hello = clf.predict(1527006867)
+    print(hello)
+
+
+def retrainers():
     print()
 
 
-
-def retrainers ():
-    print()
+if __name__ == '__main__': predictions()
