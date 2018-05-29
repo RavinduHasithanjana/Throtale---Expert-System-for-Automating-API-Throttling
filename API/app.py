7e35b8de437ca24759234274722565201f949f9f
RavinduHasithanjana <ravinduceylofana@outlook.com>
2018-05-30 00:35:39 +0530

    added the api parts

--- API/app.py
+++ API/app.py
@@ -1,14 +1,24 @@
 import os
+import datetime
+import time
 from flask import Flask
 from flask import render_template
 from sklearn.externals import joblib
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    strptime = datetime.datetime.strptime
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%d/%b/%Y:%H:%M:%S')
+    t1 = strptime(st, "%d/%b/%Y:%H:%M:%S")
+    print(t1.month)
+    print(st)
     clf = joblib.load('Final.pkl')
     # print("work")
-    preictioMade = clf.predict([[16., 12., 22., 12., 24.]])
+    preictioMade = clf.predict([[t1.day, t1.month, t1.hour, t1.minute, t1.second]])
+    # day,month,hour,min,second
     print("work")
     return str(preictioMade)
 
